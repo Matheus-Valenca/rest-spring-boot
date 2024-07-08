d098e9e5ccbd9e46f0cbdbd7d9be154fb3130fe0
matheus <matheusvalencasilva2022@gmail.com>
2024-07-08 18:34:47 -0300

    inserindo conexao com banco de dados mysql utilizando jpa

--- PrimeirosPassosSpring/rest-spring-boot/src/main/java/com/example/demo/controllers/PersonController.java
+++ PrimeirosPassosSpring/rest-spring-boot/src/main/java/com/example/demo/controllers/PersonController.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Person;
@@ -21,38 +25,34 @@ public class PersonController {
 	private PersonServices service;
 	
 	
-	@RequestMapping(method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Person> findAll()  {
 		return service.findAll();
 	}
 	
-	@RequestMapping(value = "/{id}", method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(
-			@PathVariable(value = "id")String  id)  {
-		
-		
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(@PathVariable(value = "id")Long  id)  {
 		return service.findById(id);
 	}
 	
-	@RequestMapping(method=RequestMethod.POST, 
-			produces = MediaType.APPLICATION_JSON_VALUE, 
+	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, 
 			consumes = MediaType.APPLICATION_JSON_VALUE)
 	public Person create(
 			@RequestBody Person person)  {
 		return service.create(person);
 	}
 	
-	@RequestMapping(method=RequestMethod.PUT, 
-			produces = MediaType.APPLICATION_JSON_VALUE, 
+	@PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, 
 			consumes = MediaType.APPLICATION_JSON_VALUE)
 	public Person update(
 			@RequestBody Person person)  {
 		return service.update(person);
 	}
 	
-	@RequestMapping(value = "/{id}", method=RequestMethod.DELETE)
-	public void delete(@PathVariable(value = "id")String  id)  {
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id")Long  id)  {
 		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 	
--- PrimeirosPassosSpring/rest-spring-boot/src/main/java/com/example/demo/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ PrimeirosPassosSpring/rest-spring-boot/src/main/java/com/example/demo/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.example.demo.exceptions.ExceptionResponse;
-import com.example.demo.exceptions.UnsupportedMathOperationException;
+import com.example.demo.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 @RestController
@@ -23,10 +23,10 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
-	@ExceptionHandler(UnsupportedMathOperationException.class)
-	public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request){
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(Exception ex, WebRequest request){
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
-		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
 	}
 
 }
