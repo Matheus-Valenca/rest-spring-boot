cc69fdc564c9030fe6a52df4028622a1e1b71af6
matheus <matheusvalencasilva2022@gmail.com>
2024-07-12 17:11:24 -0300

    adicionando migrations utilizando flyway

---
+++ PrimeirosPassosSpring/rest-spring-boot/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Recife', 'Matheus', 'Male', 'Valenca'),
+	(2, 'Tokyo - Japao', 'Leonardo', 'Male', 'Da vinci'),
+	(4, 'Kentucky - US', 'Muhammad', 'male', 'Ali'),
+	(6, 'Mzevo - South Africa', 'Nelson', 'male', 'Mandela'),
+	(7, 'Smiljan -  Croatia', 'Nikola', 'male', 'Tesla');
+
+
