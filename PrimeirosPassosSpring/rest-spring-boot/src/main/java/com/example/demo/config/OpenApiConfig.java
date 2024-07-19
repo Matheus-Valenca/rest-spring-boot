6e42f4051f1a7177dea7ff63152dc22ab358936a
matheus <matheusvalencasilva2022@gmail.com>
2024-07-19 10:49:43 -0300

    inserindo swagger na api

---
+++ PrimeirosPassosSpring/rest-spring-boot/src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenApiConfig {
+	
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+					.title("RESTful API with Java and Spring Boot")
+					.version("v1")
+					.description("Some description about your API")
+					.termsOfService("https://pub.erudio.com.br/meus-cursos\"")
+					.license(
+						new License()
+							.name("Apache 2.0")
+							.url("https://pub.erudio.com.br/meus-cursos")
+						)
+				);
+	}
+
+}
